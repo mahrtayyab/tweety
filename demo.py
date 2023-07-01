5b640909a00b8b5d6e5d9e04c3905708a33c8336
bengabp <bengabdev4@gmail.com>
2023-07-01 13:24:17 +0100

    Added __name__ check in demo.py

--- demo.py
+++ demo.py
@@ -2,24 +2,25 @@ from src.tweety.bot import Twitter
 import json
 from env import cookies
 
-app = Twitter(cookies=cookies)
-tweet_id = "1674458209577496577"
-limit = 3000
+if __name__ == "__main__":
+	app = Twitter(cookies=cookies)
+	tweet_id = "1674458209577496577"
+	limit = 3000
 
-print("Scraping retweets ...")
+	print("Scraping retweets ...")
 
-total_users = 0
-for retweet in app.tweet_retweeters(tweet_id):
-	total_users += 1
-	print(f"Usernames [{total_users}] => ", retweet["legacy"]["screen_name"])
-	if total_users >= limit:
-		break
+	total_users = 0
+	for retweet in app.tweet_retweeters(tweet_id):
+		total_users += 1
+		print(f"Usernames [{total_users}] => ", retweet["legacy"]["screen_name"])
+		if total_users >= limit:
+			break
 
-print("Scraping likes ...")
+	print("Scraping likes ...")
 
-total_users = 0
-for like in app.tweet_likes(tweet_id):
-	total_users += 1
-	print(f"Usernames [{total_users}] => ", like["legacy"]["screen_name"])
-	if total_users >= limit:
-		break
+	total_users = 0
+	for like in app.tweet_likes(tweet_id):
+		total_users += 1
+		print(f"Usernames [{total_users}] => ", like["legacy"]["screen_name"])
+		if total_users >= limit:
+			break
