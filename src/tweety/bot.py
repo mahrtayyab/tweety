f774b334a8da659deb092770148c77fac952f7ac
Tayyab Kharl <tayyabmahr@gmail.com>
2023-08-26 08:05:19 +0500

    Update bot.py

--- src/tweety/bot.py
+++ src/tweety/bot.py
@@ -228,18 +228,15 @@ class BotMethods:
         try:
             for entry in r['data']['threaded_conversation_with_injections_v2']['instructions'][0]['entries']:
                 if str(entry['entryId']).split("-")[0] == "tweet":
-<<<<<<< Updated upstream
+
                     raw_tweet = entry['content']['itemContent']['tweet_results']['result']
                     if raw_tweet.get('tweet'):
                         raw_tweet = raw_tweet['tweet']
                     
                     if raw_tweet.get('rest_id') == str(tweetId):
-=======
-
-                    raw_tweet = entry['content']['itemContent']['tweet_results']['result']['tweet']
+                        raw_tweet = entry['content']['itemContent']['tweet_results']['result']['tweet']
 
                     if raw_tweet['rest_id'] == str(tweetId):
->>>>>>> Stashed changes
                         return Tweet(raw_tweet, self.request, r)
         except KeyError:
             raise InvalidTweetIdentifier(144, "StatusNotFound", r)
